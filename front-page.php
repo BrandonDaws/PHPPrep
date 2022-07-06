83563d23e55ed5f0cbb5c7db697fc2dbc7be03bb
Brandon Velarde <bvelarde@Brandons-MBP.attlocal.net>
2022-07-06 15:53:27 -0500

    added header.php file

--- front-page.php
+++ front-page.php
@@ -1,42 +1,6 @@
-<!DOCTYPE html>
-<html lang="en-us">
 
-    <head>
-        <meta charset="UTF-8" />
-        
-        <title>Horiseon</title>
-		<?php
-		wp_head(  );
-		?>
-		
-    </head>
-
-    <body>
-
-        <!--Added header element-->
-        <header>
-            <h1>
-                <a href="/"> Horiseon</a>
-            </h1>
-
-            <!--Added nav element-->
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#search-engine-optimization">Search Engine Optimization</a>
-                    </li>
-                    <li>
-                        <a href="#online-reputation-management">Online Reputation Management</a>
-                    </li>
-                    <li>
-                        <a href="#social-media-marketing">Social Media Marketing</a>
-                    </li>
-                </ul>
-            </nav>
-            
-        </header>
 <?php
-$hero_img = "http://test.local/wp-content/uploads/2022/07/digital-marketing-meeting-scaled.jpg";
+get_header();
 ?>
     
         <div class="hero">
---
+++ header.php
@@ -0,0 +1,42 @@
+
+
+<html lang="en-us">
+
+    <head>
+        <meta charset="UTF-8" />
+        
+        <title>Horiseon</title>
+		<?php
+		wp_head(  );
+		?>
+		
+    </head>
+
+    <body>
+
+        <!--Added header element-->
+        <header>
+            <h1>
+                <a href="/"> Horiseon</a>
+            </h1>
+
+            <!--Added nav element-->
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#search-engine-optimization">Search Engine Optimization</a>
+                    </li>
+                    <li>
+                        <a href="#online-reputation-management">Online Reputation Management</a>
+                    </li>
+                    <li>
+                        <a href="#social-media-marketing">Social Media Marketing</a>
+                    </li>
+                </ul>
+            </nav>
+            
+        </header>
+
+
+
+	
